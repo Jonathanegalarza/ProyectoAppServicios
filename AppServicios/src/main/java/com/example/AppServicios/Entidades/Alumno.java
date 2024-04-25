72e22aec666b34cf252f027b39f398ba3b30f0bb
Jonathanegalarza <129909416+Jonathanegalarza@users.noreply.github.com>
2024-04-24 23:08:34 -0300

    commit 5
    cambios de cliente por alumno

---
+++ AppServicios/src/main/java/com/example/AppServicios/Entidades/Alumno.java
@@ -0,0 +1,21 @@
+
+package com.example.AppServicios.Entidades;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+
+public class Alumno extends Persona{
+  /*
+   La anotación @OneToMany con mappedBy = "alumno" indica que la relación está mapeada
+   por la propiedad alumno en la clase Comentario. Esto significa que en la clase Comentario,
+    debe haber una propiedad alumno que mapee la relación inversa.
+   */
+    @OneToMany(mappedBy = "alumno", cascade = CascadeType.ALL)
+    private List<Comentario> comentarios;
+
+}
--- AppServicios/src/main/java/com/example/AppServicios/Repositorios/AlumnoRepositorio.java
+++ AppServicios/src/main/java/com/example/AppServicios/Repositorios/AlumnoRepositorio.java
@@ -1,7 +1,7 @@
 
 package com.example.AppServicios.Repositorios;
 
-import com.example.AppServicios.Entidades.Cliente;
+import com.example.AppServicios.Entidades.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ClienteRepositorio extends JpaRepository<Cliente, Long>{
+public interface AlumnoRepositorio extends JpaRepository<Alumno, Long>{
     
     @Query ("SELECT c FROM Cliente c WHERE c.email = :email")
-    public Cliente buscarPorEmail(@Param("email")String email);
+    public Alumno buscarPorEmail(@Param("email")String email);
     boolean existsByDni(String dni);
     boolean existsByEmail(String email);
     
